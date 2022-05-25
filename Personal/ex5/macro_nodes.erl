3dbebe98379ddea85d014a7ca8726f593a4990a3
DannMaj <alexbearning@gmail.com>
2022-05-25 14:42:55 +0700

    Задание к занятию #5
    -Выполнил задание к занятию
    -Заметка: не работает создание узла через net_kernelstart/2

--- Personal/ex5/macro_nodes.erl
+++ Personal/ex5/macro_nodes.erl
@@ -12,15 +12,29 @@
 %Не до конца полял задание №1 и просто переопределил вызов функции
 -define(macro(Base, Power), math:pow(Base, Power)).
 %% API
--export([main/0, listener/0, sender/0]).
+-export([main/0, listener/0, sender/1]).
 
 main() ->
-  io:format("Try our macro:~p~n",[?macro(4,10)]).
+  io:format("Try our macro:~p~n",[?macro(4,10)]),
+  io:format("Create listener node...~nPlease open another termanal and call sender()~n"),
+  listener(),
+  io:format("All right!~n").
 
 listener() ->
+  net_kernel:start([lsnr,shortnames]),
+  register(lsnr, self()),
   receive
-    {SNDR,Msg} -> io:format("They say: ~s~n",[Msg]), SNDR ! {self(), "I'm get your message, thanks!"}
-  end.
+    {SNDR,Msg} -> ok = io:format("They say: ~s~n",[Msg]),
+      SNDR ! {self(), "I'm get your message, thanks!"}
+  end, ok.
 
-sender() ->
-  net_kernel:start(sndr,#{name_domain => shortnames}).
+sender(Text) ->
+  net_kernel:start([sndr,shortnames]),
+  net_kernel:connect_node('lsnr@dannmaj-ubnt'),
+  register(sndr, self()),
+  {lsnr, 'lsnr@dannmaj-ubnt'} ! {self(), Text},
+  receive
+    {_LSNR, Msg} -> ok=io:format("~s~n",[Msg])
+  end,
+  unregister(sndr),
+  net_kernel:stop().
