dc8c14900dfadeffe9d1929ee39f1c0b172c6765
DannMaj <alexbearning@gmail.com>
2022-05-26 10:28:07 +0700

    Переход КА на gen_statem
    Начал переписывать КА из занятия #6 под окружение gen_statem

--- Personal/ex6/rpc_fsm.erl
+++ Personal/ex6/rpc_fsm.erl
@@ -7,11 +7,13 @@
 %%% Created : 25. май 2022 14:48
 %%%-------------------------------------------------------------------
 -module(rpc_fsm).
+-behaviour(gen_statem).
 -author("Dann Maj").
 
 %% API
 -export([main/1, readf/1]).
--export([init/0, wait/0, open/1, read/1, send/1]).
+-export([init/0, wait/3, open/1, read/1, send/1]).
+-export([terminate/3,code_change/4,init/1,callback_mode/0]).
 
 main(0) ->
   io:format("Please, create node on another terminal %get%@%system_name%~n"),
@@ -41,15 +43,6 @@ init() ->
   io:format("Init FSM for reading files~n"),
   wait().
 
-wait() ->
-  receive
-    {PID, FileName} -> io:format("OP!~n"),open({PID,FileName});
-    Data -> io:format("Data:~p!~n",[Data]), wait()
-  after 5000 ->
-    io:format("I'm still wait!~n"),
-    wait()
-  end.
-
 open({PID, FileName}) ->
   {Stat, IoD} = file:open(FileName, [read]),
   if Stat == ok ->
@@ -67,3 +60,67 @@ read({PID, IoD}) ->
 send({PID, Number}) ->
   PID ! {self(), {number, Number}},
   wait().
+
+name() -> pushbutton_statem. % The registered server name
+
+%% API.  Функции изменения состояния
+start() ->
+  gen_statem:start({local,name()}, ?MODULE, [], []).
+push() ->
+  gen_statem:call(name(), push),
+  get().
+get() ->
+  gen_statem:call(name(), get_data),
+  find().
+find() ->
+  gen_statem:call(name(), get_number),
+  answer().
+answer() ->
+  gen_statem:call(name(), answer).
+stop() ->
+  gen_statem:stop(name()).
+
+%% Обязательные функции обратного вызова
+terminate(_Reason, _State, _Data) ->
+  void.
+code_change(_Vsn, State, Data, _Extra) ->
+  {ok,State,Data}.
+init([]) ->
+  State = off, Data = 0,
+  {ok,State,Data}.
+callback_mode()
+  -> state_functions.
+
+%%% Изменение состояний
+wait({call,From}, get_disc, Data) ->
+  receive
+    {PID, FileName} -> {next_state,open,{PID,FileName},[{reply,From,open}]};
+    _ -> {keep_state,Data}
+  after 10000 ->
+    {keep_state,Data}
+  end;
+wait(EventType, EventContent, Data) ->
+  handle_event(EventType, EventContent, Data).
+
+open({call,From}, get_data, Data) ->
+  {next_state,read,Data+1,[{reply,From,read}]};
+open(EventType, EventContent, Data) ->
+  handle_event(EventType, EventContent, Data).
+
+read({call,From}, answer, Data) ->
+  {next_state,send,Data+1,[{reply,From,send}]};
+read(EventType, EventContent, Data) ->
+  handle_event(EventType, EventContent, Data).
+
+send({call,From}, send, {PID, Number}) ->
+  PID ! {self(), {number, Number}},
+  {next_state,wait,[],[{reply,From,wait}]};
+send(EventType, EventContent, Data) ->
+  handle_event(EventType, EventContent, Data).
+
+%% Обработка состояний
+handle_event({call,From}, get_count, Data) ->
+  {keep_state,Data,[{reply,From,Data}]};
+handle_event(_, _, Data) ->
+  %% Ignore all other events
+  {keep_state,Data}.
