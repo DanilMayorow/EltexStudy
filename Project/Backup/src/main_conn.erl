0c6f77abd59939453a722f6736719dc8c3f340a6
DannMaj <alexbearning@gmail.com>
2022-06-23 17:14:00 +0700

    Продолжение тестирования NKSIP

--- Project/Backup/src/main_conn.erl
+++ Project/Backup/src/main_conn.erl
@@ -1,11 +1,12 @@
 -module(main_conn).
 
--export([test/0]).
+-export([test/0, invite/3]).
 
--define(DOMAIN, "192.168.2.36").
+-define(IP, "192.168.2.36").
+-define(DOMAIN, "ltx").
 
 test() ->
-    Client1 = string:concat("sip:1000@", ?DOMAIN),
+    Client1 = string:concat("sip:1000@", ?IP),
     nksip:start_link(client1, 
         #{sip_from => Client1,
           plugins => [nksip_uac_auto_auth],
@@ -15,15 +16,29 @@ test() ->
     PBX_ID = string:concat("sip:", ?DOMAIN),
     nksip_uac:register(client1, PBX_ID,
         [{sip_pass, "12345"}, contact, {meta, ["contact"]}]),
-    
-    Client2 = string:concat("sip:1001@", ?DOMAIN),
-    nksip_uac:invite(client1, Client2,
-        [{add, "x-nk-op", ok}, {add, "x-nk-prov", true},
-         {add, "x-nk-sleep", 8000},
-         auto_2xx_ack,
-         {sip_pass, "12345"} %, {route, "<sip:192.168.2.2;lr>"}
-        ]),
-    
-    % nksip_uac:bye(DlgId, []),
+    io:format("Register~n"),
+    timer:sleep(120000),
+    io:format("Register test end~n"),
+    %Client2 = string:concat("sip:1001@", ?IP),
+    %InviteOptions = [{add, "x-nk-op", ok}, {add, "x-nk-prov", true},
+    %  {add, "x-nk-sleep", 8000},
+    %  auto_2xx_ack,
+    %  {sip_pass, "12345"}
+    %],
+  %invite(5, Client2, InviteOptions),
+  nksip:stop(client1).
 
-    {ok, self()}.
+invite(0, _, _) ->
+  erlang:error(noinvite);
+invite(Acc, Client2, InviteOps) when Acc > 0 ->
+  case  nksip_uac:invite(client1, Client2, InviteOps) of
+    {ok, 200, [{dialog, DlgId}]} ->
+      io:format("Success ~p try~n",[Acc]),
+      timer:sleep(10000),
+      nksip_uac:bye(DlgId, []),
+      {ok, self()};
+    _Error ->
+      io:format("Fail on ~p try, wait and repeat~n",[Acc]),
+      timer:sleep(4000),
+      invite(Acc - 1, Client2, InviteOps)
+  end.
